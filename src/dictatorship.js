27921152564aebb6b64436c271aee7f65041b089
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-06-04 14:00:43 -0300

    added support for mac-os

--- src/dictatorship.js
+++ src/dictatorship.js
@@ -3,26 +3,37 @@
 var exec = require('child_process').exec;
 
 function returnInt(element){
-    return parseInt(element, 10);
+    return parseInt(element.trim(), 10);
+}
+
+function execute(command, rpid, done){
+    exec(command, function(err, data){
+        if(err){
+            return done(); // err: no pids
+        }
+
+        data.match(rpid).map(returnInt).forEach(function(pid){
+            process.stdout.write('overthrowing pid ' + pid + '...');
+            process.kill(pid);
+            console.log('done');
+        });
+
+        done();
+    });
 }
 
 module.exports = {
     overthrow: function(port, done) {
-        var grep = 'netstat -n -a -o | grep -i listening | grep :' + port,
-            rpid = /\d+$/gim;
+        var command, rpid;
 
-        exec(grep, function(err, data){
-            if(err){
-                return done(); // err: no pids
-            }
+        if(process.platform === 'darwin'){ // mac os
+            command = 'lsof -i tcp:' + port + ' | grep node | grep -i listen';
+            rpid = / \d+ /gm;
+        }else{
+            command = 'netstat -n -a -o | grep -i listening | grep :' + port;
+            rpid = /\d+$/gm;
+        }
 
-            data.match(rpid).map(returnInt).forEach(function(pid){
-                process.stdout.write('overthrowing pid ' + pid + '...');
-                process.kill(pid);
-                console.log('done');
-            });
-
-            done();
-        });
+        execute(command, rpid, done);
     }
 };
